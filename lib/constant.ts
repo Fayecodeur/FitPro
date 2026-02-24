29d947b414a6857f5f1aa0a3a49761656a2a9d2a
Demba Faye <fayecodeur99@gmail.com>
2026-02-24 18:30:22 +0000

    Ajout des liens

---
+++ lib/constant.ts
@@ -0,0 +1,12 @@
+interface navItems {
+  label: string;
+  href: string;
+}
+
+export const NAV_ITEMS: navItems[] = [
+  { label: "Accueil", href: "/" },
+  { label: "Programmes", href: "/programmes" },
+  { label: "Coachs", href: "/coachs" },
+  { label: "Blog", href: "/blog" },
+  { label: "Contact", href: "/contact" },
+];
