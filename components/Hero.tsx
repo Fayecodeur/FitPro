78bd59f0367349627bd6476772d2f5ba87b2bada
Demba Faye <fayecodeur99@gmail.com>
2026-04-18 15:44:32 +0000

    reset

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,46 +1,5 @@
-import Image from "next/image";
 import React from "react";
-import { Button } from "./ui/button";
 
 export default function Hero() {
-  return (
-    <section className="h-screen py-24 relative flex justify-center items-center overflow-hidden">
-      <div className="absolute inset-0">
-        <Image
-          src="/hero-image.jpg"
-          fill
-          alt="hero image"
-          className="object-cover object-top"
-        />
-      </div>
-      <div className="absolute inset-0 bg-black/70"></div>
-      <div className="relative z-50 max-w-4xl text-center px-4  text-white ">
-        <div>
-          <h1 className="text-5xl md:text-6xl font-heading font-black leading-tight mb-4">
-            Transformez votre corps,{" "}
-            <span className="text-primary">Transformer votre vie</span>
-          </h1>
-          <p className="text-xl md:2xl max-w-2xl text-center mx-auto font-light leading-relaxed mb-8 ">
-            Découvrez nos programmes de musculation et fitness personnalisés
-            pour atteindre vos objectifs rapidement et en toute sécurité.
-          </p>
-          <div className="flex flex-col justify-center md:flex-row gap-4">
-            <Button
-              size={"lg"}
-              className="bg-primary hover:bg-primary/90 px-8 text-md font-semibold transition-transform duration-300 hover:scale-105"
-            >
-              Rejoindre maintenant
-            </Button>
-            <Button
-              size={"lg"}
-              variant={"outline"}
-              className="bg-transparent px-8 text-md font-semibold transition-transform duration-300 hover:scale-105"
-            >
-              Voir les plans
-            </Button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  return <div>Hero</div>;
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,46 +1,5 @@
-import Container from "@/components/Container";
-import { NAV_ITEMS } from "../lib/constant";
-import Link from "next/link";
-import { Button } from "./ui/button";
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-md border-b border-border transition-colors duration-300">
-      <Container>
-        <div className="h-16 flex justify-between items-center">
-          {/* Logo */}
-          <div>
-            <Link href={"/"}>
-              <Image
-                alt="Logo"
-                src={"/logo.png"}
-                width={150}
-                height={150}
-                className="object-cover"
-              />
-            </Link>
-          </div>
-          {/* Nav Desktop */}
-          <div className="hidden sm:block">
-            <div className="flex items-baseline space-x-4">
-              {NAV_ITEMS.map((item) => (
-                <Link
-                  className="text-foreground  hover:text-primary font-semibold px-3 py-2 transition-colors duration-300"
-                  key={item.label}
-                  href={item.href}
-                >
-                  {item.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-          {/* Cta */}
-          <div className="hidden sm:block">
-            <Button>Rejoindre</Button>
-          </div>
-        </div>
-      </Container>
-    </nav>
-  );
+export default function Navbar() {
+  return <div>Navbar</div>;
 }
